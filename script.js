05d566f4ebad1ca5c02ecf69efd846256112a9de
Killian Chamberlain <killian7195@gmail.com>
2022-01-04 20:48:14 -0600

    Removing unneccesary Code

--- script.js
+++ script.js
@@ -147,35 +147,5 @@
      document.querySelector('#clear-button').addEventListener('click', ev => {
        initHistory();
      })
+
     
-    // Backgrounds for later additions
-    switch (main) {
-      case "Snow":
-        document.getElementById("wrapper-bg").style.backgroundImage =
-          "url('https://mdbgo.io/ascensus/mdb-advanced/img/snow.gif')";
-        break;
-      case "Clouds":
-        document.getElementById("wrapper-bg").style.backgroundImage =
-          "url('https://mdbgo.io/ascensus/mdb-advanced/img/clouds.gif')";
-        break;
-      case "Fog":
-        document.getElementById("wrapper-bg").style.backgroundImage =
-          "url('https://mdbgo.io/ascensus/mdb-advanced/img/fog.gif')";
-        break;
-      case "Rain":
-        document.getElementById("wrapper-bg").style.backgroundImage =
-          "url('https://mdbgo.io/ascensus/mdb-advanced/img/rain.gif')";
-        break;
-      case "Clear":
-        document.getElementById("wrapper-bg").style.backgroundImage =
-          "url('https://mdbgo.io/ascensus/mdb-advanced/img/clear.gif')";
-        break;
-      case "Thunderstorm":
-        document.getElementById("wrapper-bg").style.backgroundImage =
-          "url('https://mdbgo.io/ascensus/mdb-advanced/img/thunderstorm.gif')";
-        break;
-      default:
-        document.getElementById("wrapper-bg").style.backgroundImage =
-          "url('https://mdbgo.io/ascensus/mdb-advanced/img/clear.gif')";
-        break;
-    }
